298e45f562f181937759a6070e896b9b33195dea
suyash101101 <143525373+suyash101101@users.noreply.github.com>
2024-05-07 03:12:35 +0530

    index.js

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 var randomNumber1= Math.floor(Math.random()*6)+1;
-var randomImage = "images/dice"+randomNumber1+".png";
+var randomNumber2= Math.floor(Math.random()*6)+1;
+var randomImage1 = "images/dice"+randomNumber1+".png";
+var randomImage2 = "images/dice"+randomNumber2+".png";
 var image1 = document.querySelectorAll("img")[0];
-image1.setAttribute("src" , randomImage);
+image1.setAttribute("src" , randomImage1);
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src" , randomImage2);
+if (randomNumber1>randomNumber2){
+document.querySelector("h1").innerHTML="Player1 wins";
+}
+else if(randomNumber1<randomNumber2){
+    document.querySelector("h1").innerHTML="Player2 wins";  
+}
+else {
+    document.querySelector("h1").innerHTML="Just try again guys";
+}
 
